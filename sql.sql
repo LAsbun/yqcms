62d07317113ed5c1cfd9f45e19ad9d601b317122
LAsbun <794866972@qq.com>
2016-05-22 14:39:13 +0800

    完成数据库设计 201605221221

--- sql.sql
+++ sql.sql
@@ -3,12 +3,12 @@
 # 数据表列名  数据表_列名
 # 
 
-create database if not exists project;
+create database if not exists yqcms;
 
-use project;
+use yqcms;
 
 #创建产品分类 product_category == protype
-create table if not exists project_protype(
+create table if not exists yqcms_protype(
 	protype_id int not null auto_increment primary key,
 	protype_name varchar(30) not null,
 	protype_createTime timestamp default current_timestamp 
@@ -16,7 +16,7 @@ create table if not exists project_protype(
 );
 
 #创建产品
-create table if not exists project_product(
+create table if not exists yqcms_product(
 	product_id int not null auto_increment primary key,
 	product_name varchar(50) not null,
 	product_content text,
@@ -24,24 +24,24 @@ create table if not exists project_product(
 	on update current_timestamp,
 	product_protypeId int not null,
 	foreign key (product_id) 
-	references project_protype(protype_id)
+	references yqcms_protype(protype_id)
 	on delete cascade
 );
 
 #产品图片 (与产品是多对一)
-create table if not exists project_image(
+create table if not exists yqcms_image(
 	image_id int not null auto_increment primary key,
 	image_name varchar(64) not null,
 	image_productId int not null,
 	image_createTime timestamp default current_timestamp 
 	on update current_timestamp,
 	foreign key (image_productId) 
-	references project_product(product_id)
+	references yqcms_product(product_id)
 	on delete cascade
 );
 
 #其他属性 (与产品是多对一)
-create table if not exists project_attr(
+create table if not exists yqcms_attr(
 	attr_id int not null primary key auto_increment,
 	attr_name varchar(64) not null,
 	attr_content text,
@@ -49,11 +49,11 @@ create table if not exists project_attr(
 	on update current_timestamp,
 	attr_productId int not null,
 	foreign key(attr_productId) 
-	references project_product(product_id)
+	references yqcms_product(product_id)
 );
 
 #新闻分类
-create table if not exists project_newstype(
+create table if not exists yqcms_newstype(
 	newstype_id int not null auto_increment primary key,
 	newstype_name varchar(30) not null,
 	newstype_createTime timestamp default current_timestamp 
@@ -61,7 +61,7 @@ create table if not exists project_newstype(
 );
 
 #新闻
-create table if not exists project_news(
+create table if not exists yqcms_news(
 	news_id int not null primary key auto_increment,
 	news_name varchar(128) not null,
 	news_content text,
@@ -70,10 +70,10 @@ create table if not exists project_news(
 );
 
 #其他模块
-create table if not exists project_other(
+create table if not exists yqcms_other(
 	other_id int not null primary key auto_increment,
 	other_name varchar(64) not null,
 	other_content text,
 	other_createTime timestamp default current_timestamp 
-	on update current_timestamp,
-)
+	on update current_timestamp
+);
